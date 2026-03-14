a85c7aa49f85fa58952f3f9c91c52d752e1081b0
Marcus Grönnå <mgronna@gmail.com>
2026-03-14 18:07:42 +0100

    Refactor: return train model instead of dto

--- Infrastructure/TrainRepository.cs
+++ Infrastructure/TrainRepository.cs
@@ -5,10 +5,8 @@ namespace Infrastructure;
 
 public class TrainRepository : ITrainRepository
 {
-    public async Task<TrainResponse> GetTrainAsync()
+    public async Task<Train> GetTrainAsync()
     {
-        Train t = new Train { Id = Guid.NewGuid(), Model = "X60", Number = "6066" };
-
-        return new TrainResponse(Id: t.Id, Model: t.Model, Number: t.Number);
+        return new Train { Id = Guid.NewGuid(), Model = "X60", Number = "6066" };
     }
 }
