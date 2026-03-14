db5d4b48ddae00a109ab2b249ec4fc572bac2d02
Marcus Grönnå <mgronna@gmail.com>
2026-03-14 17:31:10 +0100

    Add: train repository for creating new train and returning info about it through a DTO.

---
+++ Infrastructure/TrainRepository.cs
@@ -0,0 +1,14 @@
+﻿using Application;
+using Domain;
+
+namespace Infrastructure;
+
+public class TrainRepository : ITrainRepository
+{
+    public async Task<TrainResponse> GetTrainAsync()
+    {
+        Train t = new Train { Id = Guid.NewGuid(), Model = "X60", Number = "6066" };
+
+        return new TrainResponse(Id: t.Id, Model: t.Model, Number: t.Number);
+    }
+}
