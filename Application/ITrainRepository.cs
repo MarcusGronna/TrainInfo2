9e86266015c08c57932560b1626ffb0f3c2355f0
Marcus Grönnå <mgronna@gmail.com>
2026-03-14 18:08:06 +0100

    Add: service to handle mapping from domain model of train to dto

--- Application/ITrainRepository.cs
+++ Application/ITrainRepository.cs
@@ -4,5 +4,5 @@ namespace Application;
 
 public interface ITrainRepository
 {
-    Task<TrainResponse> GetTrainAsync();
+    Task<Train> GetTrainAsync();
 }
---
+++ Application/TrainService.cs
@@ -0,0 +1,23 @@
+﻿using Domain;
+
+namespace Application;
+
+public class TrainService
+{
+    private readonly ITrainRepository _trainRepo;
+
+    public TrainService(ITrainRepository trainRepo)
+    {
+        _trainRepo = trainRepo;
+    }
+
+    public async Task<TrainResponse> GetTrainResponseAsync()
+    {
+        Train t = await _trainRepo.GetTrainAsync();
+
+        return new TrainResponse( 
+            Id : t.Id, 
+            Model : t.Model,
+            Number : t.Number);
+    }
+}
