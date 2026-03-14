686bb336e799447175a91905bcb190be5c35002c
Marcus Grönnå <mgronna@gmail.com>
2026-03-14 17:28:22 +0100

    Add: Train Entity

---
+++ Domain/Entities.cs
@@ -0,0 +1,8 @@
+﻿namespace Domain;
+
+public class Train
+{
+    public Guid Id { get; set; }
+    public string Number { get; set; } = String.Empty;
+    public string Model { get; set; } = String.Empty;
+}
