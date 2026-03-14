d715410bd061207ec65de80a6724bf0d10631c14
Marcus Grönnå <mgronna@gmail.com>
2026-03-14 17:32:32 +0100

    Add: controller for returning train DTO at HttpGet
    Also add scalar in Program.cs. Allso add dependency injection for the ITrainRepository in Promgram.cs,

---
+++ Api/Controllers/Trains.cs
@@ -0,0 +1,24 @@
+﻿using Application;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers;
+
+[Route("api/trains")]
+[ApiController]
+public class TrainsController : ControllerBase
+{
+    private readonly ITrainRepository _trainRepo;
+
+    public TrainsController(ITrainRepository trainRepo)
+    {
+        _trainRepo = trainRepo;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<TrainResponse>> GetTrains()
+    {
+        TrainResponse response = await _trainRepo.GetTrainAsync();
+        
+        return Ok(response);
+    }
+}
