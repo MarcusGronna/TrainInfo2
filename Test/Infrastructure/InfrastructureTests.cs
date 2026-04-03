8ecd930c974fb10a8f6a199c0428e1c10c0ddbec
Marcus Grönnå <mgronna@gmail.com>
2026-04-03 08:18:48 +0200

    add: created first test for Infrastructure/repository
    Created a test to se if the GetTrainAsync delivers the hard coded train from the method.

---
+++ Test/Infrastructure/InfrastructureTests.cs
@@ -0,0 +1,23 @@
+﻿using Application.Interfaces;
+using Domain;
+using Infrastructure;
+
+namespace Tests.Infrastructure
+{
+    public class InfrastructureTests
+    {
+        [Fact]
+        public async Task GetTrainAsync_IsCalled_ShouldReturnTrainNumber6066()
+        {
+            // Arrange
+            ITrainRepository repo = new TrainRepository();
+
+            // Act
+            Train train = await repo.GetTrainAsync();
+            string number = train.Number;
+
+            // Assert
+            Assert.Equal("6066", number);
+        }
+    }
+}
