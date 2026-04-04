e990c1521edd5e9ed7f6d6dc3108a38697b80095
Marcus Grönnå <mgronna@gmail.com>
2026-04-04 10:47:39 +0200

    refactor: moved for better repo structure

---
+++ Infrastructure/Repositories/TrainRepository.cs
@@ -0,0 +1,12 @@
+﻿using Application.Interfaces;
+using Domain;
+
+namespace Infrastructure.Repositories;
+
+public class TrainRepository : ITrainRepository
+{
+    public async Task<Train> GetTrainAsync()
+    {
+        return new Train { Id = Guid.NewGuid(), Model = "X60", Number = "6066" };
+    }
+}
