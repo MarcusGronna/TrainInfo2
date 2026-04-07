39aabd6c716f490e8fbcb5d3e67a889d19311ec3
Marcus Grönnå <mgronna@gmail.com>
2026-04-07 16:05:40 +0200

    add: HttpGet by id
    Use an Id in form of Guid to get a specific train by id

--- Api/Controllers/TrainsController.cs
+++ Api/Controllers/TrainsController.cs
@@ -26,12 +26,18 @@ public class TrainsController : ControllerBase
         return Ok(response);
     }
 
+    [HttpGet("{id}")]
+    public async Task<ActionResult<TrainResponse>> GetTrainById(Guid id)
+    {
+        var response = await _trainService.GetTrainResponseByIdAsync(id);
+
+        return Ok(response);
+    }
+
     [HttpPost]
     public async Task<ActionResult<TrainResponse>> CreateTrain(TrainRequest request)
     {
         TrainResponse response = await _trainService.CreateTrainAsync(request); 
-
-
 
         return CreatedAtAction(nameof(GetTrains), response);
     }
