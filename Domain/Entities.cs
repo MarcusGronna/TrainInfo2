1affdab5dd59cc65c90448b822efae9f4ed690de
Marcus Grönnå <mgronna@gmail.com>
2026-04-12 09:03:47 +0200

    refactor: update train entity with set methods

--- Domain/Entities.cs
+++ Domain/Entities.cs
@@ -3,6 +3,26 @@
 public class Train
 {
     public Guid Id { get; set; }
-    public string Number { get; set; } = String.Empty;
-    public string Model { get; set; } = String.Empty;
+    public string Number { get; set; }
+    public string Model { get; set; }
+
+    public Train()
+    {
+    }
+
+    public Train(string number, string model)
+    {
+        Number = number;
+        Model = model;
+    }
+
+    public void SetModel(string model)
+    {
+        Model = model;
+    }
+
+    public void SetNumber(string number)
+    {
+        Number = number;
+    }
 }
