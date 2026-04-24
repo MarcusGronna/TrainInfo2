4624cca893d8b9b5497f36b650060b8bb9ae0271
Marcus Grönnå <mgronna@gmail.com>
2026-04-24 16:13:23 +0200

    add: test for controller - should return 404
    Added test file and new test. This is to ensure correct error handling at the top level of the controller.

---
+++ Test/Api/ControllerTests.cs
@@ -0,0 +1,35 @@
+﻿using Api.Controllers;
+using Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System.Data;
+
+namespace Tests.Api;
+
+public class ControllerTests
+{
+    private readonly Mock<ITrainService> _serviceMock;
+    private readonly TrainsController _controller;
+
+    public ControllerTests()
+    {
+        _serviceMock = new Mock<ITrainService>();
+        _controller = new TrainsController(_serviceMock.Object);
+    }
+
+    [Fact]
+    public async Task ShouldReturn404_WhenResourceNotFound()
+    {
+        // Arrange
+        var id = Guid.NewGuid();
+        _serviceMock
+            .Setup(s => s.GetTrainResponseByIdAsync(id))
+            .ThrowsAsync(new RowNotInTableException());
+
+        // Act
+        var result = await _controller.GetTrainById(id);
+
+        // Assert
+        Assert.IsType<NotFoundResult>(result.Result);
+    }
+}
