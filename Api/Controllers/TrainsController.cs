cefcb6709b3f76577f5097307a2028438372b8f5
Marcus Grönnå <mgronna@gmail.com>
2026-04-24 16:17:02 +0200

    refactor: Implemented try - catch for the GetTrainById endpoint

--- Api/Controllers/TrainsController.cs
+++ Api/Controllers/TrainsController.cs
@@ -1,6 +1,7 @@
 ﻿using Application.Dtos;
 using Application.Interfaces;
 using Microsoft.AspNetCore.Mvc;
+using System.Data;
 
 namespace Api.Controllers;
 
@@ -26,9 +27,16 @@ public class TrainsController : ControllerBase
     [HttpGet("{id}")]
     public async Task<ActionResult<TrainResponse>> GetTrainById(Guid id)
     {
-        var response = await _trainService.GetTrainResponseByIdAsync(id);
+        try
+        {
+            var response = await _trainService.GetTrainResponseByIdAsync(id);
 
-        return Ok(response);
+            return Ok(response);
+        }
+        catch (RowNotInTableException)
+        {
+            return NotFound();
+        }
     }
 
     [HttpPost]
@@ -36,7 +44,7 @@ public class TrainsController : ControllerBase
     {
         TrainResponse response = await _trainService.CreateTrainAsync(request); 
 
-        return CreatedAtAction(nameof(GetTrains), response);
+        return CreatedAtAction(nameof(GetTrainById), new { id = response.Id }, response);
     }
 
     [HttpPatch("{id}")]
