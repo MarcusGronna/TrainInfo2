e95e06c77a6dbc5c81efeb34ca890e9b5d37662c
Marcus Grönnå <mgronna@gmail.com>
2026-05-17 13:15:20 +0200

    refactor(trains): allow update by id to return null

--- Application/Interfaces/ITrainService.cs
+++ Application/Interfaces/ITrainService.cs
@@ -7,5 +7,5 @@ public interface ITrainService
     Task<List<TrainResponse>> GetTrainsAsync();
     Task<TrainResponse?> GetTrainByIdAsync(Guid id);
     Task<TrainResponse> CreateTrainAsync(TrainRequest request);
-    Task<TrainResponse> UpdateTrainByIdAsync(TrainUpdateRequest request, Guid id);
+    Task<TrainResponse?> UpdateTrainByIdAsync(TrainUpdateRequest request, Guid id);
 }
